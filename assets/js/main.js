55dedbd0552212c9c1bd25079ec96d004066390b
riquelmy <riquelmyor@gmail.com>
2023-08-20 19:04:28 -0300

    Adding gradient to pokemon's background and a load button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+const pokemonOl = document.getElementById('pokemonList');
+const load_sequence = document.getElementById('next_page');
+const limit = 5;
+let offset = 0;
+
 function convertPokemonTypestoLi(pokemonTypes) {
     return pokemonTypes.map((typeSlot) => `<li class="type ${typeSlot.type.name}">${typeSlot.type.name}</li>`)
 }
@@ -6,7 +11,7 @@ function convertPokemonToHTML(pokemon){
     console.log(pokemon.types[0].type.name);
     return `
     <li class="pokemon ${pokemon.types[0].type.name}">
-    <span class="number">#${pokemon.order}</span>
+    <span class="number">#${pokemon.id}</span>
     <span class="poke_name">${pokemon.name}</span>
     <div class="detail">  
         <ol class="types">
@@ -18,13 +23,34 @@ function convertPokemonToHTML(pokemon){
     `
 }
 
-const pokemonOl = document.getElementById('pokemonList');
+    // pokeApi.getPokemons().then((pokemonList) => {
+        
+    //     for (let i = 0; i<pokemonList.length; i++) {
+    //         var pokemon = pokemonList[i];
+    //         pokemonOl.innerHTML += convertPokemonToHTML(pokemon);
+    //     }
+    // })
 
-    pokeApi.getPokemons().then((pokemonList) => {
+
+function loadPokemonItems(offset, limit) {
+    
+    pokeApi.getPokemons(offset, limit).then((pokemonList) => {
         
         for (let i = 0; i<pokemonList.length; i++) {
             var pokemon = pokemonList[i];
             pokemonOl.innerHTML += convertPokemonToHTML(pokemon);
         }
     })
+
+}
+loadPokemonItems(offset, limit);
+console.log(offset);
+
+load_sequence.addEventListener('click', () => {
+    offset += limit;
+    console.log(offset);
+    loadPokemonItems(offset, limit);
+})
+
+
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,7 +3,7 @@ const pokeApi = {}
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url).then((response) => response.json())
 }
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
     .then((response) => response.json())
