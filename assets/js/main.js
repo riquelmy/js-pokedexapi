f77fd37b327f61df88945fcb529a9215d7f10d71
riquelmy <riquelmyor@gmail.com>
2023-08-20 17:01:58 -0300

    Fully integrated and styled pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
 function convertPokemonTypestoLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+    return pokemonTypes.map((typeSlot) => `<li class="type ${typeSlot.type.name}">${typeSlot.type.name}</li>`)
 }
 
 function convertPokemonToHTML(pokemon){
+    console.log(pokemon.types[0].type.name);
     return `
-    <li class="pokemon">
+    <li class="pokemon ${pokemon.types[0].type.name}">
     <span class="number">#${pokemon.order}</span>
     <span class="poke_name">${pokemon.name}</span>
     <div class="detail">  
         <ol class="types">
             ${convertPokemonTypestoLi(pokemon.types).join('')}
         </ol>   
-         <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+         <div class="ball"><img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}"></div>
     </div>
     </li>
     `
