32d2b3255e2f5dea73268f857ce0ecb4666a6841
riquelmy <riquelmyor@gmail.com>
2023-08-20 14:46:11 -0300

    Linked the other functionalities to the Pokedex API

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,15 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+pokeApi.getPokemons = (offset = 0, limit = 50) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests)=> Promise.all(detailRequests))
+    .then((pokemonDetails) => (pokemonDetails))
     .catch((error) => console.error(error))
 }
